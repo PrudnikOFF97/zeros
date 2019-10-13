57a7f3b4c0d5eb77c3b0e563ef6ac240169f0279
PrudnikOFF97 <48676900+PrudnikOFF97@users.noreply.github.com>
2019-10-13 23:24:32 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,69 @@
 module.exports = function zeros(expression) {
-  // your solution
+  let factors = expression.split('*');
+  let fives = 0;
+  let twos = 0;
+  let value = 0;
+  for (let i = 0; i < factors.length; i++) {
+    let tmp_arr = factors[i].split('');
+    if (tmp_arr[tmp_arr.length-1] == '!' && tmp_arr[tmp_arr.length-2] == '!') {
+      if (Number(tmp_arr.slice(0, tmp_arr.length-2).join('')) % 2 == 0) {
+        for (let j = 2; j <= Number(tmp_arr.slice(0, tmp_arr.length-2).join('')); j+=2) {
+          if (j % 5 == 0) {
+            value = j;
+            while (value % 5 == 0){
+              fives+=1;
+              value /= 5;
+            }
+            value = 0;  
+          }
+          if (j % 2 == 0) {
+            value = j;
+            while (value % 2 == 0){
+              twos+=1;
+              value /= 2;
+            }
+            value = 0;  
+          }
+        }
+      }
+      else{
+        for (let j = 1; j <= Number(tmp_arr.slice(0, tmp_arr.length-2).join('')); j+=2) {
+          if (j % 5 == 0) {
+            value = j;
+            while (value % 5 == 0){
+              fives+=1;
+              value /= 5;
+            }
+            value = 0;  
+          }
+        }
+      }
+    }
+    if (tmp_arr[tmp_arr.length-1] == '!' && tmp_arr[tmp_arr.length-2] != '!') {
+      for (let j = 1; j <= Number(tmp_arr.slice(0, tmp_arr.length-1).join('')); j++) {
+        if (j % 5 == 0) {
+          value = j;
+          while (value % 5 == 0){
+            fives+=1;
+            value /= 5;
+          }
+          value = 0;
+        }
+        if (j % 2 == 0) {
+          value = j;
+          while (value % 2 == 0){
+            twos+=1;
+            value /= 2;
+          }
+          value = 0;
+        }
+      }
+    }    
+  }
+  if (fives < twos) {
+    return fives;
+  }
+  else{
+    return twos;
+  }
 }
